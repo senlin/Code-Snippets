7db16ae4d12c66cd4f7e5bd4745c176868300449
Piet Bos <senlinonline@gmail.com>
2011-09-24 15:01:34 +0800

    commit
    wpconfig.php

---
+++ ROOT/wpconfig.php
@@ -0,0 +1,6 @@
+define('WP_POST_REVISIONS', false);
+define('WP_MEMORY_LIMIT', '128M');
+define('AUTOSAVE_INTERVAL', 300); // 300 seconds is 5 mins
+define('WP_CACHE', true); // only when caching plugin is used
+/** define('TEMPLATEPATH', '/absolute/path/to/wp-content/themes/active-theme'); (optional) */
+/** define('STYLESHEETPATH', '/absolute/path/to/wp-content/themes/active-theme'); (optional) */
