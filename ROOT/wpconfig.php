2282093e15075e1ea1f1db4db04fae4d4354cfef
Piet Bos <senlinonline@gmail.com>
2011-09-24 15:03:36 +0800

    commit
    added snippet

--- ROOT/wpconfig.php
+++ ROOT/wpconfig.php
@@ -1,6 +1,12 @@
+<?php
 define('WP_POST_REVISIONS', false);
 define('WP_MEMORY_LIMIT', '128M');
 define('AUTOSAVE_INTERVAL', 300); // 300 seconds is 5 mins
 define('WP_CACHE', true); // only when caching plugin is used
 /** define('TEMPLATEPATH', '/absolute/path/to/wp-content/themes/active-theme'); (optional) */
 /** define('STYLESHEETPATH', '/absolute/path/to/wp-content/themes/active-theme'); (optional) */
+
+/** Note that if you are on a shared-server the permissions of your wp-config.php should be 750. It means that no other user will be able to read your database username and password. If you have FTP or shell access, do the following:
+chmod 750 wp-config.php
+*/
+?>
