7eb8b94c6020724aec947b2682d8db0669c4150c
Piet Bos <webmaster@senlinonline.com>
2012-12-11 23:32:43 +0800

    new posts function

---
+++ Functions/Posts/remove-private-protected-from-private-protected-post-titles.php
@@ -0,0 +1,12 @@
+<?php // Remove "Private" or "Protected" from Private or Protected Post Titles
+// via http://www.viper007bond.com/2011/12/20/how-to-remove-or-change-private-or-protected-from-wordpress-post-titles/
+
+add_filter( 'private_title_format', 'yourprefix_private_title_format' );
+function yourprefix_private_title_format( $format ) {
+    return '%s';
+}
+
+add_filter( 'protected_title_format', 'yourprefix_protected_title_format' );
+function yourprefix_protected_title_format( $format ) {
+    return '%s';
+} 
