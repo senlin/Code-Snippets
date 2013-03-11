cf1f622d435d9d99f66a8149b92b23067bb47ffd
Piet Bos <webmaster@senlinonline.com>
2013-03-11 15:40:08 +0800

    improve function

--- Functions/General/add-favicon-to-site.php
+++ Functions/General/add-favicon-to-site.php
@@ -1,17 +1,11 @@
 <?php
-// add a favicon to your site I
-function blog_favicon() {
-	echo '<link rel="Shortcut Icon" type="image/x-icon" href="'.get_bloginfo('wpurl').'/favicon.ico" />';
-}
-add_action('wp_head', 'blog_favicon');
-
-// add favicon to your site II
+// add a favicon to your site
 function add_favicon() {
-	echo '<link rel="shortcut icon" href="<?php echo bloginfo("stylesheet_directory") ?>/images/favicon.ico"/>';
+	echo '<link rel="shortcut icon" href="' . get_stylesheet_directory_uri() . '/images/favicon.ico" />';
 }
 add_action('wp_head', 'add_favicon');
 
-// add favicon to your site III: use Gravatar as favicon (source: http://wp-snippets.com/2129/gravatar-as-favicon/)
+// add favicon to your site II: use Gravatar as favicon (source: http://wp-snippets.com/2129/gravatar-as-favicon/)
 //STEP 1: add to functions.php
 function GravatarAsFavicon() {
 //We need to establish the hashed value of your email address
@@ -19,13 +13,7 @@ function GravatarAsFavicon() {
 	echo 'http://www.gravatar.com/avatar/' . $GetTheHash . '?s=16';
 }
 
-//STEP 2: add to header.php, before </head> closing tag ?>
+?>
+<!-- STEP 2: add to header.php, before </head> closing tag --> 
 <link rel="shortcut icon" href="<?php GravatarAsFavicon(); ?>" />
-
 <?php
-
-// add favicon to your site IV
-function my_favicon() { ?>
-<link rel="shortcut icon" href="yourimagepathgoeshere" >
-<?php }
-add_action('wp_head', 'my_favicon');
