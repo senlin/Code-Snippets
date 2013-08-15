7ed231264cdfcf324b17ee2aa4ab6a41a789cdae
Piet Bos <webmaster@senlinonline.com>
2013-08-15 11:35:34 +0800

    add plugins folder with new security rule

---
+++ Functions/Plugins/prevent-direct-access-to-files.php
@@ -0,0 +1,4 @@
+<?php
+// Add this line to your plugin to prevent direct access to it and with that revealing its path
+// source: http://mikejolley.com/2013/08/keeping-your-shit-secure-whilst-developing-for-wordpress/ point #2
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
