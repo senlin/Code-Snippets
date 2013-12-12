58a0f7fe079efbd6927f96a78ebd83b65c8fcc7e
Piet Bos <webmaster@senlinonline.com>
2013-12-12 08:42:25 +0800

    clean up code

--- Functions/Navigation/add-searchbox-navigation.php
+++ Functions/Navigation/add-searchbox-navigation.php
@@ -1,13 +1,13 @@
 <?php
 // ADD SEARCH BOX TO NAVIGATION
 function so_add_search_box($items, $args) {
-ob_start();
-get_search_form();
-$searchform = ob_get_contents();
-ob_end_clean();
-$items .= '<li class="nav_search">' . $searchform . '</li>';
-return $items;
+	ob_start();
+	get_search_form();
+	$searchform = ob_get_contents();
+	ob_end_clean();
+	$items .= '<li class="nav_search">' . $searchform . '</li>';
+	return $items;
 }
-add_filter('wp_nav_menu_items','so_add_search_box', 10, 2);
+add_filter( 'wp_nav_menu_items','so_add_search_box', 10, 2 );
 // END ADD SEARCH BOX
 ?>
