3f45254132f8718e7d5fc20438ea78a79dc716df
Piet Bos <webmaster@senlinonline.com>
2013-12-20 18:20:14 +0800

    updated: better body class

--- Functions/General/better-body-class.php
+++ Functions/General/better-body-class.php
@@ -1,41 +1,63 @@
 <?php
-	// Body Class Function
-	// source: http://www.mimoymima.com/2013/01/lab/better-body-class-function-wordpress/
+/**
+ * Add our own classes to the body_class function
+ * adapted from: http://www.mimoymima.com/2013/01/lab/better-body-class-function-wordpress/
+ * 
+ * @20131220: This in an update to the earlier code, where it was need to change the call to the body_class.
+ * with the snippet below however that is no longer necessary and as an additional advantage it also keeps the
+ * original WordPress body class which may (or may not) come in handy one day.
+ */
 
-function so_body_classes() {
+function so_body_classes( $classes ) {
 
     global $post;
 
- // echo some of these things
-    if ( is_category() ) { echo 'page_category' . ' '; }
-    elseif ( is_search() ) { echo 'page_search' . ' '; }
-    elseif ( is_tag() ) { echo 'page_tag' . ' '; }
-    elseif ( is_home() ) { echo 'page_home' . ' '; }
-    elseif ( is_404() ) { echo 'page_404' . ' '; }
-
-    // echo page_(page name)
-    if( is_page() ) {
-        $pn = $post->post_name;
-        echo 'page_'.$pn.' ';
+	// return some of these things
+    if ( is_category() ) {
+    	$classes[] =  'page_category';
+    } elseif ( is_search() ) {
+    	$classes[] = 'page_search';
+    } elseif ( is_tag() ) {
+    	$classes[] = 'page_tag';
+    } elseif ( is_home() ) {
+    	$classes[] = 'page_home';
+    } elseif ( is_404() ) {
+    	$classes[] = 'page_404';
     }
 
-    // echo parent_(parent name)
+    // return page_(page name)
+    if ( is_page() ) {
+        $pn = $post->post_name;
+        $classes[] = 'page_' . $pn;
+    }
+
+    // return parent_(parent name)
     $post_parent = get_post( $post->post_parent );
     $parentSlug = $post_parent->post_name;
     
     if ( is_page() && $post->post_parent ) {
-            echo 'parent_' . $parentSlug .' ';
+		$classes[] = 'parent_' . $parentSlug;
     }
 
-    // echo template_(template name)
+    // return template_(template name)
     $temp = get_page_template();
     if ( $temp != null ) {
         $path = pathinfo( $temp );
         $tmp = $path['filename'] . '.' . $path['extension'];
         $tn= str_replace( '.php', '', $tmp );
-        echo 'template_' . $tn;
+        $classes[] = 'template_' . $tn;
     }
+    
+    // if WPML has been installed return the language code
+    if ( in_array( 'sitepress-multilingual-cms/sitepress.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	    if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
+		    $lang = 'lang-' . ICL_LANGUAGE_CODE;
+		    $classes[] = $lang;
+	    }
+    }
+    
+    return $classes;
+    
 }
 
-// class in header.php: ?>
-<body class='<?php so_body_classes(); ?>'>
+add_filter( 'body_class', 'so_body_classes' );
