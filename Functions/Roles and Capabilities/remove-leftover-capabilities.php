4e01997577ce01c62df8275c25d10a56cca58784
Piet Bos <info@senlinonline.com>
2014-06-05 02:54:55 +0800

    Remove Leftover Capabilities
    New function in new folder Roles and Capabilities

---
+++ Functions/Roles and Capabilities/remove-leftover-capabilities.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Function to Remove Capabilities that are left over from plugins or from your own trials
+ *
+ * Instructions:
+ * 1. change the sample capabilities on line 14 with the capabilities you want to remove
+ * 2. save the function (ibn your functions.php or functionality file/plugin
+ * 3. optional: upload the file (if not in local environment)
+ * 4. remove the function/file, the work is done
+ *
+ * @source: //chrisburbridge.com/delete-unwanted-wordpress-custom-capabilities/
+ */
+
+function so_remove_caps() {
+
+    $delete_caps = array(
+    	'edit_issues', 'publish_issues', 'edit_other_issues', 'read_private_issues','delete_issue', 'edit_issue'
+    );
+
+    global $wp_roles;
+
+    foreach ( $delete_caps as $cap ) {
+
+        foreach ( array_keys( $wp_roles->roles ) as $role ) {
+
+            $wp_roles->remove_cap( $role, $cap );
+
+        }
+
+    }
+
+}
+
+add_action( 'admin_init', 'so_remove_caps' );
