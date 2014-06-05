90eb18ac9e81a1751bfec03988fee925b349cbb4
Piet Bos <senlin@users.noreply.github.com>
2014-06-05 19:38:42 +0800

    change line number

--- Functions/Roles and Capabilities/remove-leftover-capabilities.php
+++ Functions/Roles and Capabilities/remove-leftover-capabilities.php
@@ -3,7 +3,7 @@
  * Function to Remove Capabilities that are left over from plugins or from your own trials
  *
  * Instructions:
- * 1. change the sample capabilities on line 14 with the capabilities you want to remove
+ * 1. change the sample capabilities on line 17 with the capabilities you want to remove
  * 2. save the function (ibn your functions.php or functionality file/plugin
  * 3. optional: upload the file (if not in local environment)
  * 4. remove the function/file, the work is done
