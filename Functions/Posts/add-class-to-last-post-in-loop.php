91ee4bf816f2ad684deb39eec1780b763071b1ec
Piet Bos <info@senlinonline.com>
2014-06-25 22:22:39 +0800

    update to add class to last post in Loop

--- Functions/Posts/add-class-to-last-post-in-loop.php
+++ Functions/Posts/add-class-to-last-post-in-loop.php
@@ -8,3 +8,19 @@ function so_last_post_class($classes){
 	return $classes;
 }
 add_filter('post_class', 'so_last_post_class');
+
+/**
+ * Add "end" to last Post in the Loop
+ * For example when using Foundation for Sites
+ * 
+ * @source: //wordpress.stackexchange.com/a/7382/2015
+ */
+
+add_filter('post_class', function($classes){
+  global $wp_query;
+
+  if(($wp_query->current_post + 1) == $wp_query->post_count)
+    $classes[] = 'end';
+
+  return $classes;
+});
