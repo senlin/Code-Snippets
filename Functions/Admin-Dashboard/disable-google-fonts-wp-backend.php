9f31283d2426ccf38c5ad85ee19164b7dc518a71
Piet Bos <info@senlinonline.com>
2015-01-14 10:31:17 +0800

    disable loading Google fonts in WP backend

---
+++ Functions/Admin-Dashboard/disable-google-fonts-wp-backend.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Disable Loading of Google Fonts in WordPress Backend.
+ *
+ * @source: http://netzklad.de/2014/04/google-fonts-im-wordpress-backend-ohne-plugin-deaktivieren/
+ */
+
+function so_disable_google_fonts_wp_backend( $styles ) {
+    
+    // Open Sans always gets loaded in backend, independent on which theme has been activated
+    $styles->add( 'open-sans', '' );
+    
+    // These fonts are added by the default themes and are therefore optional
+    $styles->add( 'twentyfifteen-fonts', '' );
+    $styles->add( 'twentyfourteen-lato', '' );
+    $styles->add( 'twentythirteen-fonts', '' );
+    $styles->add( 'twentytwelve-fonts', '' );
+}
+
+add_action( 'wp_default_styles', 'so_disable_google_fonts_wp_backend', 5 );
