ac5bc492da891c5221aee3f90531ec0355740753
Piet Bos <info@senlinonline.com>
2015-06-04 21:15:50 +0800

    fix typo replace-default-site-description snippet

--- Functions/General/replace-default-site-description.php
+++ Functions/General/replace-default-site-description.php
@@ -3,8 +3,7 @@
  * Useful for theme development
  * If you want to change the default site description of "Just another WordPress website"
  *
- * add this to your functions.php file;
- * if your theme already has the `after_theme_setup`-call, then just add the `update_option` line and forget about the rest
+ * add this to your functions.php file
  */
 
 function so_replace_default_site_description() {
@@ -13,4 +12,4 @@ function so_replace_default_site_description() {
 
 }
 
-add_action( 'after_theme_setup', 'so_replace_default_site_description' );
+add_action( 'after_setup_theme', 'so_replace_default_site_description' );
