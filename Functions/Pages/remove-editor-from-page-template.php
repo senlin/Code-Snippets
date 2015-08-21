723bfc001e52f34abb3e2577115e6507297b2ab6
Piet Bos <info@senlinonline.com>
2015-08-21 16:31:38 +0800

    rewrite of remove editor function
    previous version threw PHP Notices on other pages, so was not working
    correctly. This one actually checks these things (line 32)

--- Functions/Pages/remove-editor-from-page-template.php
+++ Functions/Pages/remove-editor-from-page-template.php
@@ -1,29 +1,44 @@
 <?php
 /**
- * Disable the editor for specific page templates
+ * Remove the editor for specific page templates
  * Especially useful when using [Meta Boxes](http://metabox.io) to fully customise the UX
  *
- * Usage: adjust the template name (and folder) on line 23
+ * Usage: adjust the template name (and folder) on line 38
  *
- * @source: //snipplr.com/view.php?codeview&id=61412
+ * @source: //wordpress.stackexchange.com/a/91644/2015
  */
-add_action( 'admin_init', 'so_remove_editor' );
+add_action( 'init', 'remove_editor_init' );
 
-function so_remove_editor() {
-		
-	// Get the Post ID.
-	$post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
-	
-	if ( ! isset( $post_id ) ) return;
-	
-	// Get the name of the Page Template file.
-	$template_file = get_post_meta( $post_id, '_wp_page_template', true );
-	
-	// Here you want to edit the template name
-	if ( $template_file == 'templates/home.php' ) {
-	
-		remove_post_type_support( 'page', 'editor' );
-	
-	}
-	
+function remove_editor_init() {
+    // If not in the admin, return.
+    if ( ! is_admin() ) {
+       return;
+    }
+
+    // Get the post ID on edit post with filter_input super global inspection.
+    $current_post_id = filter_input( INPUT_GET, 'post', FILTER_SANITIZE_NUMBER_INT );
+    // Get the post ID on update post with filter_input super global inspection.
+    $update_post_id = filter_input( INPUT_POST, 'post_ID', FILTER_SANITIZE_NUMBER_INT );
+
+    // Check to see if the post ID is set, else return.
+    if ( isset( $current_post_id ) ) {
+       $post_id = absint( $current_post_id );
+    } else if ( isset( $update_post_id ) ) {
+       $post_id = absint( $update_post_id );
+    } else {
+       return;
+    }
+
+    // Don't do anything unless there is a post_id.
+    if ( isset( $post_id ) ) {
+       // Get the template of the current post.
+       $template_file = get_post_meta( $post_id, '_wp_page_template', true );
+
+       // Example of removing page editor for page-your-template.php template.
+       if (  'page-your-template.php' === $template_file ) {
+           remove_post_type_support( 'page', 'editor' );
+           // Other features can also be removed in addition to the editor. See: https://codex.wordpress.org/Function_Reference/remove_post_type_support.
+           remove_post_type_support( 'page', 'thumbnail' );
+       }
+    }
 }
