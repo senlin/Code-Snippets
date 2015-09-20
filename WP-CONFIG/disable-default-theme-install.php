7b9d9ac868243d148d635b90bc9a5abdce46b390
Piet Bos <info@senlinonline.com>
2015-09-20 11:06:11 +0800

    WP-CONFIG: disable default theme/plugin install

---
+++ WP-CONFIG/disable-default-theme-install.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Disable installing default themes/plugins
+ *
+ * @source: //bavotasan.com/2015/permanently-delete-default-wordpress-themes/
+ */
+
+define( 'CORE_UPGRADE_SKIP_NEW_BUNDLED', true );
