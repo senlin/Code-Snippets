2a57823eeda9694c379f015dbbf2bd9e4a8f7dd2
Piet Bos <info@senlinonline.com>
2015-09-20 11:05:34 +0800

    WP-CONFIG: define default theme

---
+++ WP-CONFIG/define-default-theme.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Define default theme, useful when building custom themes for clients
+ *
+ * change 'custom-theme-slug' for the slug of your custom theme
+ *
+ * @source: //bavotasan.com/2015/permanently-delete-default-wordpress-themes/
+ */
+
+define( 'WP_DEFAULT_THEME', 'custom-theme-slug' );
