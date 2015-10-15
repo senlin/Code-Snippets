1d34a3d8d444500dc66eba640429cf912d159ca2
Piet Bos <info@senlinonline.com>
2015-10-15 18:26:57 +0800

    extend function to hide Post, Category, Tags completely from site

--- Functions/Admin-Dashboard/adminbar-remove-new-post-link.php
+++ Functions/Admin-Dashboard/adminbar-remove-new-post-link.php
@@ -22,3 +22,39 @@ function so_remove_new_post_link() {
 	//$wp_admin_bar->remove_node( 'new-user' );
 
 }
+
+/**
+ * Further to this function there is a bit more that needs to be removed or hidden.
+ * 
+ * First we need to make sure to remove the Uncategorized category from the site. This can only be done via the Database
+ */
+ 
+ // make sure to replace wp_ with your own site's table prefix. To be sure look it up in your wp-config.php file
+ // DO THIS AT YOUR OWN RISK AND BACK UP YOUR DATABASE BEFORE YOU DO ANYTHING
+ DELETE FROM `wp_terms` WHERE `slug` LIKE '%uncategorized%';
+ 
+ /**
+ * The following function adds a stylesheet that hides Post, Category and Tags from the Nav Menu editor as well as the Customizer
+ * Below that follows a stylesheet that you can add to the css folder of your theme. If your theme does not have such a folder,
+ * you can add it yourself.
+ */
+
+add_action( 'admin_enqueue_scripts', 'so_backend_styling' );
+
+function so_backend_styling() {
+    wp_enqueue_style( 'backend-styling', get_template_directory_uri() . '/css/backend.css', array(), null );
+}
+
+/**
+ * Add to css/backend.css
+ */
+
+/* hide the Post and Category from wp-admin/nav-menus.php */
+.nav-menu-meta .add-post, 
+.nav-menu-meta .add-category,
+/* hide the Post, Catrgory and Tags from the Customizer */
+.wp-customizer #available-menu-items-post_type-post,
+.wp-customizer #available-menu-items-taxonomy-category,
+.wp-customizer #available-menu-items-taxonomy-post_tag {
+    display: none;
+}
